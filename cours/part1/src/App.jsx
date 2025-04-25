8557be91946a48567cd6cd5af60acb06a7075800
Clemsimao <simaoclement33@gmail.com>
2025-04-25 10:18:23 +0200

    exo: maj exo part 2

--- cours/part1/src/App.jsx
+++ cours/part1/src/App.jsx
@@ -1,9 +1,9 @@
-const Hello = ({ name, age }) => {
+const Hello = ({ name, age, hobby }) => {
   const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <p>Hello {name}, you are {age} years old</p>
+      <p>Hello {name}, you are {age} years old and your hobby is {hobby}</p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
@@ -12,13 +12,14 @@ const Hello = ({ name, age }) => {
 const App = () => {
   const name = 'Peter'
   const age = 10
+  const hobby = 'video games'
 
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Hello name="clem" age={26} />
+      <Hello name="Maya" age={26 + 10} hobby={hobby} />
+      <Hello name={name} age={age} hobby={"ride a bike"} />
+      <Hello name="clem" age={26} hobby={"nothing"} />
       
     </div>
   )
--- exo/part1/src/App.jsx
+++ exo/part1/src/App.jsx
@@ -1,25 +1,6 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Part = ({ name, exercises }) => {
-  return <p>{name} {exercises}</p>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {total}</p>;
-};
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
 
 const App = () => {
   const course = {
